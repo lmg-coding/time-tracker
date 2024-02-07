03da79d16d61779f2a2a33e6615ff7b344cf644e
luisya22 <lmatosgarcia2@gmail.com>
2024-02-06 23:11:28 -0400

    feat: summary

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 // Better to use something like Cobra, but we are going to build just for the sake of learning
 func main() {
+
 	// Get command
 	if len(os.Args) < 2 {
 		fmt.Println("error: no command provided")
@@ -39,6 +40,45 @@ func main() {
 		entries: make(map[string]*entry),
 	}
 
+	e1St, _ := time.ParseInLocation("2006-01-02", "2024-02-05", time.Local)
+	e1Et, _ := time.ParseInLocation("2006-01-02", "2024-02-05", time.Local)
+
+	e1 := &entry{
+		name:        "Task 1",
+		description: "Description 1",
+		startTime:   e1St,
+		endTime:     e1Et,
+	}
+
+	app.entries[e1.name] = e1
+
+	e2 := &entry{
+		name:        "Task 2",
+		description: "Description 2",
+		startTime:   time.Now().UTC(),
+		endTime:     time.Now().UTC(),
+	}
+
+	app.entries[e2.name] = e2
+
+	e3 := &entry{
+		name:        "Task 3",
+		description: "Description 3",
+		startTime:   time.Now().Add(24 * time.Hour),
+		endTime:     time.Now().Add(25 * time.Hour),
+	}
+
+	app.entries[e3.name] = e3
+
+	e4 := &entry{
+		name:        "Task 4",
+		description: "Description 3",
+		startTime:   time.Now().Add(-7 * 24 * time.Hour),
+		endTime:     time.Now().Add(-7 * 24 * time.Hour),
+	}
+
+	app.entries[e4.name] = e4
+
 	switch command {
 	case "start":
 		app.handleStart(*name, *description)
@@ -50,7 +90,6 @@ func main() {
 
 	}
 
-	fmt.Println(app.entries)
 	// Validate command
 	// Handle Command
 }
@@ -86,7 +125,49 @@ func (a *application) handleEnd(name string) error {
 
 }
 
-func (a *application) handleSummary(startTimeStr string, endTimeStr string, weekStr string, year string) {
+func (a *application) handleSummary(startTimeStr string, endTimeStr string, weekStr string, year string) error {
+	// I need a per day description of the task I've done
+
+	entries := []*entry{}
+
+	if startTimeStr != "" || endTimeStr != "" {
+		startDate, endDate, err := getDates(startTimeStr, endTimeStr)
+		if err != nil {
+			return err
+		}
+
+		entries = a.getByDateRange(startDate, endDate)
+	} else {
+		entries = a.getActualWeek()
+	}
+
+	for _, e := range entries {
+		fmt.Printf(
+			"name: %v; startTime: %v; endTime: %v\n",
+			e.name,
+			e.startTime.Format("2006-01-02"),
+			e.endTime.Format("2006-01-02"),
+		)
+	}
+
+	return nil
+}
+
+func getDates(startTimeStr string, endTimeStr string) (time.Time, time.Time, error) {
+
+	startDate, err := time.ParseInLocation("2006-01-02", startTimeStr, time.Local)
+	if err != nil {
+		return time.Time{}, time.Time{}, err
+	}
+
+	endDate, err := time.ParseInLocation("2006-01-02", endTimeStr, time.Local)
+	if err != nil {
+		return time.Time{}, time.Time{}, nil
+	}
+
+	endDate = endDate.Add(24*time.Hour - time.Nanosecond)
+
+	return startDate, endDate, err
 }
 
 type entry struct {
@@ -127,6 +208,31 @@ func (a *application) updateEntry(e *entry) error {
 	}
 
 	return nil
+}
+
+func (a *application) getByDateRange(paramStartDate time.Time, paramEndDate time.Time) []*entry {
+
+	entries := []*entry{}
+
+	for _, e := range a.entries {
+		if (paramStartDate.Before(e.startTime) || paramStartDate.Equal(e.startTime)) &&
+			(paramEndDate.After(e.endTime) || paramEndDate.Equal(e.endTime)) {
+
+			entries = append(entries, e)
+
+		}
+	}
+
+	return entries
+}
+
+func (a *application) getActualWeek() []*entry {
+	offset := -int(time.Now().Weekday())
+
+	weekStart := time.Now().AddDate(0, 0, offset).Truncate(24 * time.Hour)
+	weekEnd := weekStart.AddDate(0, 0, 4).Add(24*time.Hour - time.Nanosecond)
+
+	return a.getByDateRange(weekStart, weekEnd)
 }
 
 // Save to File
